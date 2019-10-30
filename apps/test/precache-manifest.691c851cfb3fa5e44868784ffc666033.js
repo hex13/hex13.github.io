b9af4e0c8b8e945f96fac2c0ddc1cc2c3c54330c
Łukasz Lityński <lukasz.litynski@gmail.com>
2019-10-30 16:17:13 +0100

    testing url encoding/decoding

---
+++ apps/test/precache-manifest.691c851cfb3fa5e44868784ffc666033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457b2821a69c25744bcc648f83d06c29",
+    "url": "/apps/test/index.html"
+  },
+  {
+    "revision": "4959d8963713a352329f",
+    "url": "/apps/test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "131383931a30263f09d5",
+    "url": "/apps/test/static/js/2.fac11da1.chunk.js"
+  },
+  {
+    "revision": "4959d8963713a352329f",
+    "url": "/apps/test/static/js/main.b4995b4c.chunk.js"
+  },
+  {
+    "revision": "bf8096a72966108414e3",
+    "url": "/apps/test/static/js/runtime-main.ab72d93d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/apps/test/static/media/logo.25bf045c.svg"
+  }
+]);
