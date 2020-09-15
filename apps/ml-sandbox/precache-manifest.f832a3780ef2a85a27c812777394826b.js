b7120035768176eb6494ab03dc6cffd636be0a14
Łukasz Lityński <lukasz.litynski@gmail.com>
2020-09-15 21:20:21 +0200

    ml-sandbox

---
+++ apps/ml-sandbox/precache-manifest.f832a3780ef2a85a27c812777394826b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2584658fd6679958086b6af2e5908a89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91147005027aaf0c14f",
+    "url": "/static/css/main.7504e268.chunk.css"
+  },
+  {
+    "revision": "f3741bcd40f1334bfac4",
+    "url": "/static/js/2.ff34b7a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff34b7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91147005027aaf0c14f",
+    "url": "/static/js/main.bdc7b3ae.chunk.js"
+  },
+  {
+    "revision": "6552367398949ffaaeb6",
+    "url": "/static/js/runtime-main.50c708a3.js"
+  }
+]);
